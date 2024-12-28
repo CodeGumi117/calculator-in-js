22e51718fde9be214a1c5699130a01e801369b30
CodeGumi117 <xxgumimodzxx@gmail.com>
2024-12-28 18:28:56 +0100

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,15 @@
+console.log("Welcome to a project thats been totally copied by someone!")
+ input = parseInt(prompt("numbah 1: "));
+ inputalt = parseInt(prompt("numbah 2: "));
+ inputalt2 = parseInt(prompt("operator: "));
+ if (inputalt2 === /) {
+    console.log(input / inputalt);
+    } else if (inputalt2 === +) {
+        console.log(input + inputalt);
+    } else if (inputalt2 === -) {
+        console.log(input - inputalt);
+    } else if (inputalt2 === *) {
+        console.log(input * inputalt);
+    } else {
+        console.log("bruh use a valid operator");
+ }
